695aacf4531cfd90677c38df73927ab650639d4d
DimitrisFournarakos <dfournarakos567@gmail.com>
2026-08-11 15:30:07 +0300

    Hide the .cluster-health-history index from gui

--- elasticsearch_api.py
+++ elasticsearch_api.py
@@ -194,6 +194,11 @@ def get_indices(cluster):
         if "." in display_name:
             display_name = display_name.split(".")[0]
 
+        #Εδώ είναι που κρύβω τον index που φτιάχνω για να αποθηκεύονται μέσα σε αυτόν όλα τα records για το health του cluster.
+        #Όταν δηλαδή το Cluster π.χ. αλλάξει από Green σε Yellow κλπ. + 1 record,που κρατάω μέσα στον index cluster-health-history.        
+        if index["index"] in ".cluster-health-history":
+            continue
+
         indices.append({
         "name": index["index"],
         "display_name": display_name,
