f70c40b3c15f8eb461d0dcb210ca1ff00086bbea
DimitrisFournarakos <dfournarakos567@gmail.com>
2026-08-19 16:46:28 +0300

    Fix details-title bug in add clusters panel

--- static/js/add_cluster.js
+++ static/js/add_cluster.js
@@ -10,24 +10,26 @@ document.addEventListener("DOMContentLoaded", function(){
             return;
         }
 
-        document.getElementById("details-title").innerHTML = document.getElementById("details-title").innerHTML = `<svg width="24"
-     height="23"
-     viewBox="0 0 24 24"
-     fill="none"
-     stroke="currentColor"
-     stroke-width="2.5"
-     stroke-linecap="round"
-     stroke-linejoin="round"
-     style="vertical-align:middle; position:relative; top:-4px;">
+        document.getElementById("details-title").innerHTML = `<svg width="24"
+                                                                height="23"
+                                                                viewBox="0 0 24 24"
+                                                                fill="none"
+                                                                stroke="currentColor"
+                                                                stroke-width="2.5"
+                                                                stroke-linecap="round"
+                                                                stroke-linejoin="round"
+                                                                style="vertical-align:middle; position:relative; top:-4px;">
 
-    <ellipse cx="10" cy="5" rx="6" ry="3"></ellipse>
+                                                                <ellipse cx="10" cy="5" rx="6" ry="3"></ellipse>
 
-    <path d="M4 5v7c0 1.7 2.7 3 6 3s6-1.3 6-3V5"></path>
+                                                                <path d="M4 5v7c0 1.7 2.7 3 6 3s6-1.3 6-3V5"></path>
 
-    <path d="M20 10v8"></path>
-    <path d="M16 14h8"></path>
+                                                                <path d="M20 10v8"></path>
+                                                                <path d="M16 14h8"></path>
 
-</svg> Add Cluster`;
+                                                            </svg> Add Cluster`;
+        
+        document.getElementById("details-title").innerHTML = ClusterName;
 
     });
 
